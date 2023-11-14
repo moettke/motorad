eb16257716c5b886a6b1267dedc22ade0710003c
Martin Poguntke <martin@web.de>
2023-11-14 15:45:44 +0100

    commit aus IntelliJ

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         Fahrzeug[] mA = new Fahrzeug[3];
         Motorrad m = new Motorrad(125, 100, 15);
         Pkw p = new Pkw(100, 150, 4);
+        System.out.println("BLALALALALLALA");
 
         mA[0] = m;
         mA[1] = p;
